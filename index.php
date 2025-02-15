ac1aaa05a98e7357961fb23ac1cf7dbfdf520dc7
CUFDM <contactusfordigitalmarketing@gmail.com>
2025-02-15 23:36:16 +0530

    Update CSS Files Location

--- index.php
+++ index.php
@@ -120,6 +120,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Financial Entry System - MyAccBook</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://localhost/bootstrap.min.css" rel="stylesheet">
     <style>
         html, body {
             height: 100%;
